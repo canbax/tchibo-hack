2fb42edd5eb4e0d6b50ccff7c3249348862640f3
canbax <yusufsaidcanbaz@gmail.com>
2021-01-21 21:26:33 +0300

    use pickled data for faster read, format JSON for first products page

--- data_merger.py
+++ data_merger.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 import time
 
 start_time = time.time()
@@ -15,5 +16,9 @@ while cnt <= PAGE_CNT:
 
 with open('all_products.json', 'w+') as f:
   f.write(json.dumps(all_data))
+
+# pickled data can be read nearly 2 times faster than JSON
+with open('all_products_pickle', 'wb') as f:
+  pickle.dump(all_data, f)     
 
 print(str(PAGE_CNT) + ' donwloaded in ' + str(time.time() - start_time))
--- recommender.py
+++ recommender.py
@@ -1,1 +1,19 @@
-#  we can use description, category, price and images to make a similarity matrix and recommomend the highest scoring ones
+import json
+import time
+import pickle
+# we can use description, category, price and images to make a similarity matrix and recommomend the highest scoring ones
+
+start_time = time.time()
+x = None
+with open('all_products_pickle', 'rb') as f:
+  x = pickle.load(f)
+
+# with open('all_products.json', 'r') as f:
+#   x = json.loads(f.read())
+
+print(' read all data in ' + str(time.time() - start_time))
+
+# for xi in x:
+#   print(xi['category']['google_shopping_api'])
+
+
