0bb59e6a4d61ef0ec8a21aaacfdbb25664033e0a
canbax <yusufsaidcanbaz@gmail.com>
2021-01-30 16:01:53 +0300

    read properties in a more parametric way

--- recommender.py
+++ recommender.py
@@ -49,40 +49,76 @@ def german_sentence_sim(s1: str, s2: str):
   return d1.similarity(d2)
 
 
+def get_value(xi, path: list):
+  obj = xi[path[0]]
+  for p in path[1:]:
+    obj = obj[p]
+  return obj
+
+
+def get_prop_values(xi, props):
+  l = []
+  for p in props:
+    if p['is_float']:
+      l.append(float(get_value(xi, p['path'])))
+    else:
+      l.append(get_value(xi, p['path']))
+
+  return l
+
+
+def calculate_basic_sim(product_idx: int, x):
+  prop_list = [
+      {'is_float': False, 'path': ['category', 'google_shopping_api']},
+      {'is_float': True, 'path': ['price', 'amount']}
+  ]
+  main_product = get_prop_values(x[product_idx], prop_list)
+
+  similarity2MainProduct = []
+  for i, xi in enumerate(x):
+    if i == product_idx:
+      continue
+    candidate_product = get_prop_values(xi, prop_list)
+    similarities = {}
+    for j, p in enumerate(prop_list):
+      sim = 0
+      if p['is_float']:
+        sim = num_sim(candidate_product[j], main_product[j])
+      else:
+        sim = substr_sim(candidate_product[j], main_product[j])
+      similarities['sim' + str(j)] = sim
+    similarities['product_idx'] = i
+    similarity2MainProduct.append(similarities)
+
+  # get only the top 100 similar ones because calculating similarity to each other takes 6-7 minutes for a product
+  similarity2MainProduct = sorted(similarity2MainProduct, key=lambda x: x['sim0'], reverse=True)[
+      :SENTENCE_SIM_LIMIT]
+  return similarity2MainProduct
+
+
 def recommend4(product_idx: int, x):
   """ product_idx is the index of the product to make suggestions
    x should be a list of all the products. (like products_1.json) """
-  p1 = x[product_idx]['category']['google_shopping_api']
-  p2 = float(x[product_idx]['price']['amount'])
-
   start_time = time.time()
-
-  sim4TheProduct = []
-  for idx, xi in enumerate(x):
-    if idx == product_idx:
-      continue
-    sim = substr_sim(p1, xi['category']['google_shopping_api'])
-    sim2 = num_sim(p2, float(xi['price']['amount']))
-    sim4TheProduct.append({'sim1': sim, 'sim2': sim2, 'product_idx': idx})
-
-  # get only the top 100 similar ones because calculating similarity to each other takes 6-7 minutes for a product
-  sim4TheProduct = sorted(sim4TheProduct, key=lambda x: x['sim1'], reverse=True)[
-      :SENTENCE_SIM_LIMIT]
-
+  sim4TheProduct = calculate_basic_sim(product_idx, x)
+  prop1 = ['description', 'long']
   for sim in sim4TheProduct:
-    desc1 = x[product_idx]['description']['long']
-    desc2 = x[sim['product_idx']]['description']['long']
-    sim['sim3'] = german_sentence_sim(desc1, desc2)
-    sim['recommend_score'] = sim['sim1'] * 0.13 + \
-        sim['sim2'] * 0.33 + + sim['sim3'] * 0.54
-  
-  sim4TheProduct = sorted(sim4TheProduct, key=lambda x: x['recommend_score'], reverse=True)
+    desc1 = get_value(x[product_idx], prop1)
+    desc2 = get_value(x[sim['product_idx']], prop1)
+    sim['sim2'] = german_sentence_sim(desc1, desc2)
+    sim['recommend_score'] = sim['sim0'] * 0.33 + \
+        sim['sim1'] * 0.33 + + sim['sim2'] * 0.34
 
-  webbrowser.open(x[product_idx]['image']['default'], new=2)
+  sim4TheProduct = sorted(
+      sim4TheProduct, key=lambda x: x['recommend_score'], reverse=True)
+
+  prop2 = ['image', 'default']
+  webbrowser.open(get_value(x[product_idx], prop2), new=2)
   for sim in sim4TheProduct[:10]:
-    webbrowser.open(x[sim['product_idx']]['image']['default'], new=2)
+    webbrowser.open(get_value(x[sim['product_idx']], prop2), new=2)
 
   print(sim4TheProduct[:10])
   print(' executed in ' + str(time.time() - start_time))
+
 
 recommend4(0, x)
